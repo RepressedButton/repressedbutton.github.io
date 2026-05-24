c7349dd1aeb3fa5bc5bcc3b97016e7e8b9c3fcd4
RepressedButton <102931525+RepressedButton@users.noreply.github.com>
2026-05-23 20:29:57 -0400

    Add files via upload

--- ap/client/client.js
+++ ap/client/client.js
@@ -1,23 +1,27 @@
+//import readline from "node:readline";
+
 import { Client } from "https://unpkg.com/archipelago.js/dist/archipelago.min.js";
 
-// Create a new instance of the Client class.
 const client = new Client();
+
+const sendButton = document.getElementById("sendButton");
 
 const outputBox = document.getElementById("output");
 
-// Set up an event listener for whenever a message arrives and print the plain-text content to the console.
 client.messages.on("message", (content) => {
     console.log(content);
     outputBox.value += content + "\n";
 });
 
-// Login to the server.
-// If no game is provided, client will connect in "TextOnly" mode, which is fine for this example.
 client.login(document.getElementById("server").value + ":" + document.getElementById("port").value, document.getElementById("slot").value, "", {
     password: document.getElementById("password").value,
-    slotData: true,
-    tags: ["Tracker"]
+    slotData: true
 });
 
-client.messages.say("test");
+adminConnectButton.addEventListener("click", () => {
+    client.messages.say("!admin login " + document.getElementById("adminPassword").value);
+});
 
+sendButton.addEventListener("click", () => {
+    client.messages.say("!admin /send_location " + document.getElementById("slot").value + " " + document.getElementById("input").value);
+});
